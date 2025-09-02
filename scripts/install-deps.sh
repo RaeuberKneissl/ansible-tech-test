c7c40817358be78f66c9f5f7e1bb18e74ee3d93e
Flo <2076161+RaeuberKneissl@users.noreply.github.com>
2025-09-02 15:10:58 +0200

    Direnv (#1)
    * NOREF: Add direnv
    
    * NOREF: Add direnv hook, turn brew analytics off
    
    * NOREF: Switch to zsh
    
    * NOREF: Check if direnv hook is already in .zshrc file
    
    * NOREF: make install-deps.sh using an interactive shell
    
    * NOREF: Testrun without using interactive mode in script
    
    * NOREF: Interactive mode
    
    ---------
    
    Co-authored-by: Florian Kneißl <florian.kneissl@xempus.com>

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/zsh -i
 
 # Install dependencies
 # Usage: ./install-deps.sh
 
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${(%):-%N}")" && pwd)"
 ANSIBLE_DIR="$(dirname "$SCRIPT_DIR")"
 
 echo "Installing dependencies..."
@@ -13,12 +13,30 @@ echo "========================================"
 
 cd "$ANSIBLE_DIR"
 
+if [[ $OSTYPE == 'darwin'* ]]; then
+    # Check for Homebrew and install if we don't have it
+    if ! command -v brew >/dev/null 2>&1; then
+        echo "Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        brew analytics off
+    fi
+
+    # Check for direnv and install if we don't have it
+    if ! brew ls --versions direnv > /dev/null; then
+        echo "Installing direnv..."
+        brew install direnv
+
+        if ! grep -q 'eval "$(direnv hook zsh)"' ~/.zshrc; then
+            echo "Adding direnv hook to .zshrc"
+            echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
+        fi
+        source ~/.zshrc
+    fi
+fi
+
 # Install ansible
 if [[ -f "requirements.txt" ]]; then
     echo "Installing from requirements.txt..."
-
-    python3 -m venv .venv
-    source .venv/bin/activate
     pip3 install -r requirements.txt
 else
     echo "Error: requirements.txt not found in $ANSIBLE_DIR"
@@ -27,6 +45,9 @@ fi
 
 # Install collections and roles
 if [[ -f "requirements.yml" ]]; then
+    direnv allow
+    eval "$(direnv export zsh)"
+
     echo "Installing from requirements.yml..."
     ansible-galaxy install -r requirements.yml --force
     echo ""
